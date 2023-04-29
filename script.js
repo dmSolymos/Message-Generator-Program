99053af987b92a7c29f63f92b12704669271de9c
Adam Solymos <adam.solymos77@gmail.com>
2023-04-29 18:17:55 +0200

    first part

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
-const rdm = Math.floor(Math.random(10-1)+1);
+const rdm1 = Math.floor(Math.random()*3+1);
+//const rdm2 = Math.floor(Math.random(3-1)+1);
+//const rdm3 = Math.floor(Math.random(3-1)+1);
 
+let meal = "";
+let food = "";
+let drink = "";
 
-function Message(num){
-    switch(num){
+function Message(num1){
+    switch(num1){
         case 1:
-            console.log("You get nothing");
-            break;
+            return meal ="Breakfast";
         case 2:
-            console.log("You are empty");
-            break;
+            return meal ="Lunch";
         case 3:
-            console.log("bruh");
-            break;
-        case 4:
-            console.log("The sun is shining");
-            break;
-        case 5:
-            console.log("Welcome to the jungle baby");
-            break;
-        case 6:
-            console.log("We went camping");
-            break;
-        case 7:
-            console.log("The night is young and full of terrors");
-            break;
-        case 8:
-            console.log("Sign of power show of force");
-            break;
-        case 9:
-            console.log("Seven seas");
-            break;
-        case 0:
-            console.log("This was long")
-            break;
-
+            return meal ="Dinner";
+    };/*
+    switch(num2){
+        case 1:
+            return food ="Pizza";
+        case 2:
+            return food ="Spagetthi";
+        case 3:
+            return food ="Lasagna";
+    };
+    switch(num3){
+        case 1:
+            return drink ="Coke";
+        case 2:
+            return drink="Pepsi";
+        case 3:
+            return drink="Sprite";
     }
-}
+*/
+    return meal;
+    }
 
-Message(rdm);
+    Message(rdm1);
+    console.log(meal);
+    console.log(rdm1);
+
